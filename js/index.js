6af44d9c440dc41a9a5b5cc15c6772691bb523d2
gkkary3 <81800956+gkkary3@users.noreply.github.com>
2022-05-19 23:29:39 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const toggleBtn = document.querySelector('.seconds_bar');
+const toggleBtn2 = document.querySelector('.first_bar');
+const seconds = document.querySelector('.seconds');
+const third = document.querySelector('.third');
+
+toggleBtn.addEventListener('click',()=>{
+    seconds.classList.toggle('active');
+    third.classList.toggle('exit');
+    
+});
+
+toggleBtn2.addEventListener('click',()=>{
+    
+    
+    seconds.classList.toggle('active');
+    third.classList.toggle('exit');
+    
+   
+    
+});
+
